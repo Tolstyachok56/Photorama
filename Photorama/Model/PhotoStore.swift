e9077436abbbfdb3fbda5729ce2402e03c60041b
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-06 15:10:53 +0300

    Sending the request

---
+++ Photorama/Model/PhotoStore.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Виктория Бадисова on 06.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import Foundation
+
+class PhotoStore {
+    
+    private let session: URLSession = { return URLSession(configuration: .default) }()
+    
+    func fetchInterestingPhotos() {
+        let url = FlickrAPI.interestingPhotosURL
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let jsonData = data {
+                if let jsonString = String.init(data: jsonData, encoding: .utf8) {
+                    print(jsonString)
+                }
+            } else if let requestError = error {
+                print("Error fetching interesting photos: \(requestError)")
+            } else {
+                print("Unexpected error with the request")
+            }
+        }
+        task.resume()
+    }
+    
+}
