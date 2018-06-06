b7c3e018ff453d02b14ed792db721b1d6eb3ea38
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-06 13:01:05 +0300

    Skeleton project

---
+++ Photorama/Controller/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Виктория Бадисова on 06.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
