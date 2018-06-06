feba657965d5964b14c072b2b889e989cd9ecdd6
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-06 16:54:51 +0300

    Downloading and Displaying the Image Data

--- Photorama/Model/PhotoStore.swift
+++ Photorama/Model/PhotoStore.swift
@@ -6,11 +6,20 @@
 //  Copyright © 2018 Виктория Бадисова. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 enum PhotoResult {
     case success([Photo])
     case failure(Error)
+}
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
 }
 
 class PhotoStore {
@@ -22,7 +31,9 @@ class PhotoStore {
         let request = URLRequest(url: url)
         let task = session.dataTask(with: request) { (data, response, error) in
             let result = self.processPhotoRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -32,6 +43,33 @@ class PhotoStore {
             return .failure(error!)
         }
         return FlickrAPI.photo(fromJSON: jsonData)
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData)
+        else {
+            if data == nil {
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+        return .success(image)
     }
     
 }
