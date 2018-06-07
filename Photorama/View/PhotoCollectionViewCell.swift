44752fc39d3b454cd6b90cddb46d2e3a8dda0926
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-07 13:03:27 +0300

    Creating a Custom UICollectionViewCell

---
+++ Photorama/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Виктория Бадисова on 07.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spiner: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+    
+    func update(with image: UIImage?) {
+        if let imageToDisplay = image {
+            spiner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spiner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+}
