70560f590de903feca06122c8499278103582af8
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-07 12:37:15 +0300

    Collection View Data Source

---
+++ Photorama/Model/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Виктория Бадисова on 07.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        return cell
+    }
+    
+}
